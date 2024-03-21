f8872cd7a9394dd9dfdaa48ff70a73ea3e06ff52
原心 <yunsin@vip.qq.com>
2024-03-21 16:40:34 +0800

    feat: 统一博客名

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
-export const SITE_TITLE = '岁月回首';
+export const SITE_TITLE = '原心';
 // export const SITE_DESCRIPTION = '历尽沧桑犹勤勉；岁月回溯心自安';
-export const SITE_DESCRIPTION = '已过而立当慎行，岁月回首须无憾。';
+// export const SITE_DESCRIPTION = '已过而立当慎行，岁月回首须无憾。';
+export const SITE_DESCRIPTION = '贵有恒，何必三更起五更眠';
 
 // export const SITE_DESCRIPTION = '勤勉身力行，岁月回首心无憾';
 
