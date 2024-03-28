baff44cfebcccb89ff1ad1af3554f19f4be01c55
原心 <yunsin@vip.qq.com>
2024-03-28 20:42:21 +0800

    feat: 优化页面并在底部添加备案信息

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = '原心';
+export const SITE_TITLE = '原心的个人主页';
 // export const SITE_DESCRIPTION = '历尽沧桑犹勤勉；岁月回溯心自安';
 // export const SITE_DESCRIPTION = '已过而立当慎行，岁月回首须无憾。';
 export const SITE_DESCRIPTION = '贵有恒，何必三更起五更眠';
