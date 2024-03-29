d9996b6ff07bd0b1104d5c5c8edc955a6f854c10
原心 <yunsin@vip.qq.com>
2024-03-29 13:51:42 +0800

    feat: 优化页面样式

--- uno.config.ts
+++ uno.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
     ['w-mc', {width: 'max-content'}],
     ['flex-2', {flex: '2'}],
     ['sticky-top', {position: 'sticky', 'box-shadow': '0px 8px 16px rgba(0,0,0,0.1)', 'z-index': '10'}],
+    ['main', {'min-height': 'calc(100vh - 408px)'}],
     ['sticky-bottom', {position: 'sticky', 'box-shadow': '0px -4px 8px rgba(0,0,0,0.1)', 'z-index': '10'}],
   ],
   theme: {
-
     breakpoints: {
       xxs: '0px',
       xs: '320px',
