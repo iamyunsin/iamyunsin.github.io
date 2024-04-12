91f8c487a14bbfb27a10b1ed49c06d3158a88069
原心 <yunsin@vip.qq.com>
2024-04-12 14:32:28 +0800

    fix: 构建失败问题

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,14 @@
 import rss from '@astrojs/rss';
-import sanitizeHtml from 'sanitize-html';
 import { getCollection } from 'astro:content';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
-import { SITE_TITLE, SITE_DESCRIPTION } from '@/consts';
+// import MarkdownIt from 'markdown-it';
+// const parser = new MarkdownIt();
+import { SITE } from '@/consts';
 
 export async function GET(context) {
 	const posts = (await Promise.all([getCollection('blog'), getCollection('life')])).flat().filter(({ data }) => data.draft !== true);
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: SITE.title,
+		description: SITE.description,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
